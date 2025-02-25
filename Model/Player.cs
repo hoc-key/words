596adf23ea6d1b8e6323a1be0f629529485cc617
hoc-key <65369688+hoc-key@users.noreply.github.com>
2025-02-25 18:08:30 +0300

    Add Player.cs

---
+++ Model/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Корнеев_Угадай_слово.Models
+{
+    public class Player : Person
+    {
+        public int Score { get; set; }
+    }
+}
