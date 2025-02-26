cb53bd2d6d3d268e253325e1e103f3a094195d29
hoc-key <65369688+hoc-key@users.noreply.github.com>
2025-02-26 15:28:44 +0300

    Add GuessResult.cs

---
+++ Business/GuessResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Корнеев_Угадай_слово.Business
+{
+    public enum GuessResult
+    {
+        Correct,
+        Incorrect,
+        AlreadyGuessed,
+        Invalid
+    }
+}
