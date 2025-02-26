6a6276c2b2952a71bb50918411e53b813b26e6ab
hoc-key <65369688+hoc-key@users.noreply.github.com>
2025-02-26 15:27:32 +0300

    Add GameState.cs

---
+++ Business/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Корнеев_Угадай_слово.Business
+{
+    public class GameState
+    {
+        public string WordToGuess { get; set; } = string.Empty;  // Инициализируем пустой строкой
+        public List<char> GuessedLetters { get; set; } = new List<char>(); // Инициализируем пустым списком
+        public int RemainingAttempts { get; set; }
+        public int Score { get; set; }
+        public bool IsGameOver { get; set; }
+    }
+}
