c1782e811d852c99d1959443968b87a51b7b66af
hoc-key <65369688+hoc-key@users.noreply.github.com>
2025-02-26 15:26:49 +0300

    Add GameConstants.cs

---
+++ Business/GameConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Корнеев_Угадай_слово.Business
+{
+    public static class GameConstants
+    {
+        public const int MaxAttemptsDefault = 6;
+        public const string SaveFileName = "gamestate.json";
+    }
+}
