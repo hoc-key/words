5ca859d6ce52fbb4645c925b33b8d76f8ca41528
hoc-key <65369688+hoc-key@users.noreply.github.com>
2025-02-26 15:28:09 +0300

    Add GuessEventArgs.cs

---
+++ Business/GuessEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Корнеев_Угадай_слово.Business
+{
+    public class GuessEventArgs : EventArgs
+    {
+        public char GuessedLetter { get; }
+        public GuessResult Result { get; }
+        public string CurrentWordState { get; }
+
+        public GuessEventArgs(char guessedLetter, GuessResult result, string currentWordState)
+        {
+            GuessedLetter = guessedLetter;
+            Result = result;
+            CurrentWordState = currentWordState;
+        }
+    }
+}
